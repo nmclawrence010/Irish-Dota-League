ddca74cd909825da3f7338ecf55dc255304ab223
nmclawrence010 <nmclawrence010@gmail.com>
2025-03-22 23:44:34 +0000

    Div 2 final

--- src/components/KnockoutBracket.tsx
+++ src/components/KnockoutBracket.tsx
@@ -314,7 +314,7 @@ export const KnockoutBracket: React.FC<KnockoutBracketProps> = ({ teams, divisio
       type: "match",
       data: {
         label: "👑 Champion",
-        team1: { name: "TBD" },
+        team1: { name: sortedTeams[1].name },
         stage: "champion",
       },
       position: { x: xStart + xGap * 4, y: yStart + yGap * 2 },
