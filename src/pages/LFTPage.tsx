33da86bb2790297e2dfb58e4899608c47f16cb82
nmclawrence010 <nmclawrence010@gmail.com>
2025-05-04 01:50:27 +0100

    Blocked link to LFT

--- src/pages/LFTPage.tsx
+++ src/pages/LFTPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Navigate } from "react-router-dom";
 import { LFTForm } from "../components/LFTForm";
 import { useLFTPlayers } from "../hooks/useLFTPlayers";
 import { useAuth } from "../hooks/useAuth";
